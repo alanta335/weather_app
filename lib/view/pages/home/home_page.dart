53fef36086e351f9aac49eebb9ef6967997c78e1
alanta335 <alanta335@gmail.com>
2024-08-08 19:39:15 +0530

    added weather icon in home screen

--- lib/view/pages/home/home_page.dart
+++ lib/view/pages/home/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:weather_app/viewmodel/location/location_viewmodel.dart';
-import 'package:weather_app/viewmodel/weather/weather_viewmodel.dart';
+import 'package:weather_app/core/utility/widget/weather_svg.dart';
+import 'package:weather_app/viewmodel/home/home_viewmodel.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
@@ -13,42 +13,32 @@ class HomePage extends ConsumerStatefulWidget {
 class _HomePageState extends ConsumerState<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final currentLocation = ref.watch(locationViewmodelProvider);
-
-    return Scaffold(
-      body: currentLocation.when(
-        data: (position) {
-          final lat = position.value!.latitude.toString();
-          final lon = position.value!.longitude.toString();
-
-          // Fetch weather data based on the location
-          final currentWeather = ref
-              .watch(weatherViewmodelProvider.notifier)
-              .currentWeather(lat: lat, lon: lon);
-
-          return currentWeather.when(
-            data: (weather) => Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Latitude: $lat'),
-                    const SizedBox(width: 10),
-                    Text('Longitude: $lon'),
-                  ],
-                ),
-                Text(weather.name.toString()),
-              ],
+    final currentWeather = ref.watch(homeViewmodelProvider);
+    return currentWeather.when(data: (currentWeather) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('${currentWeather.name}'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('${currentWeather.weather?.first.icon}'),
+            SizedBox(
+              height: 150,
+              child: WeatherSVG(
+                iconName: currentWeather.weather?.first.icon,
+              ),
             ),
-            loading: () => const CircularProgressIndicator(),
-            error: (error, stackTrace) =>
-                Text('Error fetching weather: $error'),
-          );
-        },
-        error: (error, stackTrace) => Text('Error fetching location: $error'),
-        loading: () => const CircularProgressIndicator(),
-      ),
-    );
+          ],
+        ),
+      );
+    }, error: (er, st) {
+      return Scaffold(
+        body: Text('error: ${er.toString()}'),
+      );
+    }, loading: () {
+      return const Scaffold(body: CircularProgressIndicator());
+    });
   }
 }
