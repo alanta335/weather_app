ccb1e912924b0aa2f887cca9425c376b2e7793ae
alanta335 <alanta335@gmail.com>
2024-08-11 11:09:02 +0530

    completed the search for place to get the weather location

--- lib/view/Search/pages/search_page.dart
+++ lib/view/Search/pages/search_page.dart
@@ -12,82 +12,79 @@ class SearchPage extends ConsumerWidget {
     final weather = ref.watch(searchViewmodelProvider);
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          height: 1200,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                // TextField for user input
-                TextField(
-                  decoration: const InputDecoration(
-                    labelText: 'Search Locations',
-                    prefixIcon: Icon(Icons.search),
-                  ),
-                  onChanged: (String value) async {
-                    await ref
-                        .read(suggestionsProvider.notifier)
-                        .updateSuggestions(location: value);
-                  },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Search Locations',
+                  prefixIcon: Icon(Icons.search),
                 ),
-                const SizedBox(height: 10),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: suggestions.length,
-                    itemBuilder: (context, index) {
-                      final suggestion = suggestions[index];
-                      return ListTile(
-                        title: Text(suggestion.name.toString()),
-                        onTap: () {
-                          ref
-                              .read(locationSearchTextProvider.notifier)
-                              .update(suggestion);
-                          ref
-                              .read(searchViewmodelProvider.notifier)
-                              .fetchLocationWeather(
-                                  lat: suggestion.lat.toString(),
-                                  lon: suggestion.lon.toString());
-                          ref
-                              .read(suggestionsProvider.notifier)
-                              .updateSuggestions(location: null);
-                        },
-                      );
-                    },
+                onChanged: (String value) async {
+                  await ref
+                      .read(suggestionsProvider.notifier)
+                      .updateSuggestions(location: value);
+                },
+              ),
+              const SizedBox(height: 10),
+              if (suggestions.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      maxHeight: suggestions.length * 55,
+                    ),
+                    child: ListView.builder(
+                      itemCount: suggestions.length,
+                      itemBuilder: (context, index) {
+                        final suggestion = suggestions[index];
+                        return ListTile(
+                          tileColor: Colors.green,
+                          title: Text(suggestion.name.toString()),
+                          onTap: () {
+                            ref
+                                .read(locationSearchTextProvider.notifier)
+                                .update(suggestion);
+                            ref
+                                .read(searchViewmodelProvider.notifier)
+                                .fetchLocationWeather(
+                                    lat: suggestion.lat.toString(),
+                                    lon: suggestion.lon.toString());
+                            ref
+                                .read(suggestionsProvider.notifier)
+                                .updateSuggestions(location: null);
+                          },
+                        );
+                      },
+                    ),
                   ),
                 ),
-                const SizedBox(height: 10),
-                weather.when(
-                  data: (_) {
-                    return _ != null
-                        ? WetherDetails(currentWeather: _)
-                        : const SizedBox();
-                  },
-                  error: (er, st) {
-                    return Scaffold(
-                      body: Text('error: ${er.toString()}'),
-                    );
-                  },
-                  loading: () {
-                    return const Scaffold(
-                      body: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            CircularProgressIndicator(),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text('Wait... loading the weather data'),
-                          ],
-                        ),
+              weather.when(
+                data: (_) {
+                  return _ != null
+                      ? WetherDetails(currentWeather: _)
+                      : const SizedBox();
+                },
+                error: (er, st) {
+                  return Text('error: ${er.toString()}');
+                },
+                loading: () {
+                  return const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircularProgressIndicator(),
+                      SizedBox(
+                        height: 10,
                       ),
-                    );
-                  },
-                ),
-              ],
-            ),
+                      Text('Wait... loading the weather data'),
+                    ],
+                  );
+                },
+              )
+            ],
           ),
         ),
       ),
